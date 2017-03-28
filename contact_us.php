0105aebc24260fa7dd5146cfe3725085e43f71d5
kalamsuthar <kalamsuthar@gmail.com>
2017-03-28 16:44:54 +0530

    Add files via upload

---
+++ contact_us.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang=en>
+<head>
+<title>The feedback form</title>
+<meta charset=utf-8>
+<link rel="stylesheet" type="text/css" href="includes.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+.cntr{
+  text-align:center;
+  }
+</style>
+</head>
+<body>
+<div id='container'>
+  <?php include("header.php"); ?>
+  <?php include("nav.php"); ?>
+  <?php include("info-col.php"); ?>
+<div id='content'>
+
+     <h2>Contact Us</h2>
+     	<div class="cntr">
+     <strong>Address:</strong> Veermata Jijabai Technological Institute</br> H R Mahajani Marg, Matunga, Mumbai, Maharashtra 400031</br>
+     <b>Tel:</b>000110101</div>
+		 <p class="cntr">
+	Email us at :<strong>kalamsuthar@gmail.com</strong></p>
+
+
+</div>
+<div id="footer">
+<?php include('footer.php'); ?>
+</div></div></div><br>
+</body>
+</html>
